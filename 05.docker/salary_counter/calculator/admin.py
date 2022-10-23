4a9346591a8b02254fa5a21c8b0b650f2ddc2f89
dotsenkois <dotsenkois@yandex.ru>
2022-10-23 20:13:41 +0700

    Перенс приложение в другой репозиторий

--- 05.docker/salary_counter/calculator/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Results, Holidays
-
-admin.site.register(Profile)
-admin.site.register(Results)
-admin.site.register(Holidays)
--- 05.docker/salary_counter/calculator/forms.py
+++
@@ -1,9 +0,0 @@
-from .models import Profile
-from django.forms import ModelForm
-
-
-class UserForm(ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['date_start', 'date_end', 'hourly_fee', 'district_coefficient',
-                  'northern_coefficient', 'award', 'extras', 'days_road', 'observation']
