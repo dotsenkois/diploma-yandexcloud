3b6c8542206ef845b0b7666bec7285c83ffc65e7
dotsenkois <dotsenkois@yandex.ru>
2022-10-24 22:08:11 +0700

    20221024

--- 04.tf_infrastructure_k8s_managed/03.k8s-subnets.tf
+++ 04.tf_infrastructure_k8s_managed/03.k8s-subnets.tf
@@ -2,16 +2,19 @@ resource "yandex_vpc_subnet" "k8s-private-zone-a" {
   v4_cidr_blocks = ["192.168.10.0/27"]
   zone           = "ru-central1-a"
   network_id =  yandex_vpc_network.k8s-network.id
+  route_table_id = yandex_vpc_route_table.lab-rt-a.id
 }
 
 resource "yandex_vpc_subnet" "k8s-private-zone-b" {
   v4_cidr_blocks = ["192.168.10.32/27"]
   zone           = "ru-central1-b"
   network_id =  yandex_vpc_network.k8s-network.id
+  route_table_id = yandex_vpc_route_table.lab-rt-a.id
 }
 
 resource "yandex_vpc_subnet" "k8s-private-zone-c" {
   v4_cidr_blocks = ["192.168.10.64/27"]
   zone           = "ru-central1-c"
   network_id =  yandex_vpc_network.k8s-network.id
+  route_table_id = yandex_vpc_route_table.lab-rt-a.id
 }
