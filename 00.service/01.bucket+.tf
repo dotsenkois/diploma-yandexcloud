e917a2f04ef1cf736c2f664fb73204da2753a68e
dotsenkois <dotsenkois@yandex.ru>
2022-11-09 14:08:01 +0000

    20221109

--- 00.service/01.bucket+.tf
+++ 00.service/01.bucket+.tf
@@ -50,12 +50,12 @@ resource "yandex_storage_object" "terraform" {
   source     = "terraform_1.3.3_linux_amd64.zip"
 }
 
-resource "yandex_storage_object" "jenkins-bkp" {
-  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket = yandex_storage_bucket.distrs-storage.id
-  key        = "jenkins-bkp.tgz"
-  source     = "jenkins-bkp.tgz"
-}
+# resource "yandex_storage_object" "jenkins-bkp" {
+#   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+#   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+#   bucket = yandex_storage_bucket.distrs-storage.id
+#   key        = "jenkins-bkp.tgz"
+#   source     = "jenkins-bkp.tgz"
+# }
 
 
